81cf9ac8fcaa4790c229a3b13b53422df7be283c
Mariana Theml <marianatheml@gmail.com>
2022-05-09 03:38:32 -0300

    feat: Add a modal to register new students

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import React, { useState } from "react";
 import './Navbar.css'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 import * as FaIcons from "react-icons/fa";
+import { TextField } from "@mui/material";
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'background.paper',
+    borderRadius: '25px',
+    boxShadow: 24,
+    p: 5,
+};
 
 function Navbar() {
     const [active, setActive] = useState("nav_menu");
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     const navToggle = () => {
         if (active === "nav_menu") {
@@ -28,14 +46,39 @@ function Navbar() {
                     </a>
                 </li>
                 <li className="nav_item_add">
-                    <a href="/">
-                        Novo aluno
-                    </a>
+                    <button onClick={handleOpen}>
+                        +
+                    </button>
                 </li>
             </ul>
             <div onClick={navToggle} className="nav_toggler">
                 <FaIcons.FaBars />
             </div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <div className="register_title">
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Cadastrar
+                        </Typography>
+                        <Typography id="modal-modal-description">
+                            Preencha todos os campos para adicionar um novo aluno ao sistema.
+                        </Typography>
+                    </div>
+                    <div className="register_fields">
+                        <TextField label="Nome" variant="standard" />
+                        <TextField label="E-mail" variant="standard" />
+                        <TextField label="CPF" variant="standard" />
+                    </div>
+                    <div className="register_button">
+                        <button>Cadastrar</button>
+                    </div>
+                </Box>
+            </Modal>
         </nav>
     );
 }
